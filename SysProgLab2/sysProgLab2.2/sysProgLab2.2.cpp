c5f02c71c591f3633af5dd49f483539c51bfd563
PolishukArtem <CazeOne1@gmail.com>
2020-05-14 17:55:55 +0300

    LoadTask

---
+++ SysProgLab2/sysProgLab2.2/sysProgLab2.2.cpp
@@ -0,0 +1,234 @@
+﻿// sysProgLab2.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "locale.h" 
+#include <stdio.h>
+#include "TCHAR.h"
+#include "windows.h"
+#include <shlwapi.h>
+#include <wchar.h>
+
+using namespace std;
+
+bool PrintDirectory(TCHAR tCurrentDirectory[]);
+bool ChangeDirectory(TCHAR* tCurrentDirectory, TCHAR* tNewPath);
+bool CopySomeFile(TCHAR* tExistFilePath, TCHAR* tNewFilePath);
+bool CreateSomeDirectory(TCHAR* tNewPath);
+bool DeleteEmptyDirectoryOrFile(TCHAR* tPathToDeleting);
+bool PrintInfoAboutFile(TCHAR* tFilePath);
+void ShowError();
+void FixPath(TCHAR tNewPath[]);
+
+//функция изменения текущей директории
+bool ChangeDirectory(TCHAR* tCurrentDirectory, TCHAR* tNewPath)
+{
+	cout << "\nOld path: ";
+	wcout << tCurrentDirectory << endl;
+	FixPath(tNewPath);
+	DWORD dw = GetFileAttributes(tNewPath);
+	if (dw != FILE_ATTRIBUTE_DIRECTORY)
+	{
+		cout << "Your path isn't for a directory" << endl;
+		return false;
+	}
+	wcscpy(tCurrentDirectory, tNewPath);
+	cout << "New path: ";
+	wcout << tCurrentDirectory << endl;
+	return true;
+}
+
+//функция вывода файлов в текущей папке
+bool PrintDirectory(TCHAR tCurrentDirectory[])
+{
+	TCHAR tBuff[MAX_PATH];
+	WIN32_FIND_DATAW id;
+	SYSTEMTIME st;
+	_sntprintf(tBuff, MAX_PATH, L"%s\\*\0", tCurrentDirectory);
+	HANDLE hFiles = FindFirstFile(tBuff, &id);
+	if (hFiles != INVALID_HANDLE_VALUE)
+	{
+		wcout << "\n" << tCurrentDirectory << endl;
+		do
+		{
+			if ((wcscmp(L".", id.cFileName) == 0) || (wcscmp(L"..", id.cFileName) == 0))
+				continue;
+			wcout << " - " << id.cFileName;
+			cout << "  (File size: " << ((id.nFileSizeHigh * MAXDWORD) + id.nFileSizeLow) / 1024 << " KB  ";
+			if (FileTimeToSystemTime(&id.ftCreationTime, &st))
+				wprintf_s(L"Creation time: %d-%d-%d, %d:%d)\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute);
+		} while (FindNextFile(hFiles, &id));
+		FindClose(hFiles);
+		return true;
+	}
+	return false;
+}
+
+//функция создания директории
+bool CreateSomeDirectory(TCHAR* tNewPath)
+{
+	FixPath(tNewPath);
+	if (!CreateDirectory(tNewPath, NULL))
+		return false;
+	return true;
+}
+
+//функция удаления
+bool DeleteEmptyDirectoryOrFile(TCHAR* tPathToDeleting) // function delete empty directory or file
+{
+	FixPath(tPathToDeleting);
+	if (!RemoveDirectory(tPathToDeleting))
+	{
+		if (!DeleteFile(tPathToDeleting))
+			return false;
+	}
+	return true;
+}
+
+//функция копирования файла
+bool CopySomeFile(TCHAR* tExistFilePath, TCHAR* tNewFilePath)
+{
+	FixPath(tExistFilePath);
+	DWORD dw = GetFileAttributes(tExistFilePath);
+	if (dw == INVALID_FILE_ATTRIBUTES)
+	{
+		cout << "No such file!" << endl;
+		return false;
+	}
+	FixPath(tNewFilePath);
+	if (!CopyFile(tExistFilePath, tNewFilePath, TRUE))
+	{
+		ShowError();
+		return false;
+	}
+	return true;
+}
+
+//вывод инфы про файл
+bool PrintInfoAboutFile(TCHAR* tFilePath)
+{
+	WIN32_FIND_DATA id;
+	SYSTEMTIME st;
+	FixPath(tFilePath);
+	if (!FindFirstFile(tFilePath, &id))
+		return false;
+	cout << "\n\t File info: "; wcout << id.cFileName << endl;
+	cout << "Alter file name: "; wcout << id.cAlternateFileName << endl;
+	cout << "file size: " << ((id.nFileSizeHigh * MAXDWORD) + id.nFileSizeLow) / 1024 << " KB" << endl;
+	if (FileTimeToSystemTime(&id.ftCreationTime, &st))
+		wprintf_s(L"Create date: %d-%d-%d, %d:%d\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute);
+	if (FileTimeToSystemTime(&id.ftLastAccessTime, &st))
+		wprintf_s(L"Last access date: %d-%d-%d, %d:%d\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute);
+	if (FileTimeToSystemTime(&id.ftLastWriteTime, &st))
+		wprintf_s(L"Last record date: %d-%d-%d, %d:%d\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute);
+	cout << "File attributes: " << endl;
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_ARCHIVE)
+		cout << " - Archive" << endl;
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_COMPRESSED)
+		cout << " - Compressed" << endl;
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_HIDDEN)
+		cout << " - Hidden" << endl;
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_NORMAL)
+		cout << " - Normal" << endl;
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_OFFLINE)
+		cout << " - Unenable file data" << endl;
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_READONLY)
+		cout << " - Readonly";
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_SYSTEM)
+		cout << " - System" << endl;
+	if (id.dwFileAttributes == FILE_ATTRIBUTE_TEMPORARY)
+		cout << " - Temporary" << endl;
+	return true;
+}
+
+int main()
+{
+	TCHAR tCurrentPath[MAX_PATH]{ '\0' };
+	GetCurrentDirectory(MAX_PATH, tCurrentPath);
+	TCHAR tCommand[MAX_PATH]{ '\0' };
+	TCHAR* pch;
+	TCHAR tCmd[MAX_PATH]{ '\0' };
+	TCHAR tFirstPath[MAX_PATH]{ '\0' };
+	TCHAR tSecondPath[MAX_PATH]{ '\0' };
+
+	for (;;) {
+		cout << "\n============= File Manager =============" << endl;
+		cout << "changedir [directoryPath] - Change directory" << endl;
+		cout << "printdir - Show curent directory's files" << endl;
+		cout << "copyfile [file] [newPathToFile] - Copy file" << endl;
+		cout << "createdir [directoryPath] - Create new directory" << endl;
+		cout << "delete [directoryPath/filePath] - Delete directory" << endl;
+		cout << "printinfo [file] - Show information about file" << endl;
+
+		cout << "Input command to work with file manager: " << endl;
+
+		fgetws(tCommand, MAX_PATH, stdin);
+		pch = _wcstok(tCommand, L" ");
+		int i = 0;
+		while (pch != NULL) {
+			if (i == 0)_tcscpy(tCmd, pch);
+			if (i == 1)_tcscpy(tFirstPath, pch);
+			if (i == 2)_tcscpy(tSecondPath, pch);
+			pch = _wcstok(NULL, L" ");
+			i++;
+		}
+		FixPath(tCmd);
+		if (_tcscmp(L"changedir", tCmd) == 0)
+		{
+			if (!ChangeDirectory(tCurrentPath, tFirstPath))
+				ShowError();
+		}
+		else if (_tcscmp(L"printdir", tCmd) == 0)
+		{
+			if (!PrintDirectory(tCurrentPath))
+				ShowError();
+		}
+		else if (_tcscmp(L"copyfile", tCmd) == 0)
+		{
+			if (!CopySomeFile(tFirstPath, tSecondPath))
+				ShowError();
+		}
+		else if (_tcscmp(L"createdir", tCmd) == 0)
+		{
+			if (!CreateSomeDirectory(tFirstPath))
+				ShowError();
+		}
+		else if (_tcscmp(L"delete", tCmd) == 0)
+		{
+			if (!DeleteEmptyDirectoryOrFile(tFirstPath))
+				ShowError();
+		}
+		else if (_tcscmp(L"printinfo", tCmd) == 0)
+		{
+			if (!PrintInfoAboutFile(tFirstPath))
+				ShowError();
+		}
+		else {
+			cout << "No such command" << endl;
+			continue;
+		}
+	}
+}
+
+//функция фикса пути
+void FixPath(TCHAR tNewPath[])
+{
+	if (wcschr(tNewPath, '\n'))
+		tNewPath[(wcschr(tNewPath, '\n') - tNewPath)] = '\0'; //изменяет последний "\n" на "\0"
+}
+
+//функция вывода ошибки
+void ShowError()
+{
+	LPVOID lpMsgBuf;
+	DWORD dwLastError = GetLastError();
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, dwLastError,
+		MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("\n%s\n", lpMsgBuf);
+	LocalFree(lpMsgBuf);
+	return;
+}
---
+++ SysProgLab2/sysproglab2/sysproglab2.cpp
@@ -0,0 +1,361 @@
+﻿// sysproglab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "windows.h"
+#include "stdio.h"
+#include "string.h"
+
+using namespace std;
+
+#define TEXTFIELD_SIZE 80
+
+CONST WCHAR RECORDFILE[] = L"RecordBase.rb";
+//Структура для записи в файл
+typedef struct
+{
+	WORD wRecordId; // запись айди
+	FILETIME fltmCreationTime; // запись тайминга создания
+	CHAR cText[TEXTFIELD_SIZE + 1]; // запись текста
+	WORD wChangeRecordCounter; // счётчик
+} USERRECORD, *P_USERRECORD;
+
+// Структура заголовка
+typedef struct
+{
+	WORD wNotNullRecordsCount; //НеНулевой счётчик
+	WORD wRecordFileSize; //размер файла
+} RECORDFILE_HEADER, *P_RECORDFILE_HEADER;
+
+bool InitProgData(HANDLE* hRecordFile, DWORD* dwFileSize, DWORD* dwCountRecords);
+bool GetHead(HANDLE hRecordFile, DWORD dwCreateOrGetHeader, P_RECORDFILE_HEADER pHeader, BOOL bClearFile);
+bool CreateRec(HANDLE hRecordsFile, DWORD* dwFileSize, CHAR* recText, DWORD* dwRecordId);
+bool WriteRecInFile(HANDLE hRecordFile, USERRECORD usRecord, DWORD dwOffset);
+bool DeleteRec(HANDLE hRecordFile, DWORD dwIdToDelete, DWORD* dwCountRecords, DWORD* dwFileSize);
+bool ShowRec(HANDLE hRecordFile, DWORD dwCountRecords);
+bool UpdateRec(HANDLE hRecordFile, DWORD  dwIdToModify);
+
+//Initialize data
+bool InitProgData(HANDLE* hRecordFile, DWORD* dwFileSize, DWORD* dwCountRecords)
+{
+	//открыть файл
+	*hRecordFile = CreateFile(RECORDFILE,
+		GENERIC_ALL,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+	if (*hRecordFile == INVALID_HANDLE_VALUE)
+		return false;
+
+	//получить размер файла
+	*dwFileSize = GetFileSize(*hRecordFile, NULL);
+	if (*dwFileSize == INVALID_FILE_SIZE)
+		return false;
+
+	if (*dwFileSize <= sizeof(RECORDFILE_HEADER))
+	{
+		RECORDFILE_HEADER header;
+		header.wNotNullRecordsCount = 0;
+		header.wRecordFileSize = sizeof(RECORDFILE_HEADER);
+		if (!GetHead(*hRecordFile, 1, &header, TRUE)) //создать заголовок
+		{
+			return false;
+		}
+		*dwFileSize = sizeof(RECORDFILE_HEADER);
+	}
+	*dwCountRecords = (*dwFileSize - sizeof(RECORDFILE_HEADER)) / sizeof(USERRECORD);
+	return true;
+}
+
+//Filling file
+bool WriteRecInFile(HANDLE hRecordFile, USERRECORD usRecord, DWORD dwOffset)
+{
+	if (SetFilePointer(hRecordFile, dwOffset, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+		return false;
+
+	DWORD dwCountWritenBytes = 0;
+	if ((WriteFile(hRecordFile, &usRecord, sizeof(usRecord), &dwCountWritenBytes, NULL) == false) || (dwCountWritenBytes != sizeof(usRecord)))
+		return false;
+
+	return true;
+}
+
+
+//Create record
+bool CreateRec(HANDLE hRecordsFile, DWORD* dwFileSize, CHAR* recText, DWORD* dwRecordId)
+{
+	BOOL isNullRecord = false;
+	USERRECORD usRecord;
+	if (strcmp(recText, "0") == 0)
+	{
+		memset(usRecord.cText, '\0', sizeof(usRecord.cText));
+		isNullRecord = true;
+	}
+	else
+	{
+		strcpy_s(usRecord.cText, TEXTFIELD_SIZE, recText);
+		usRecord.cText[TEXTFIELD_SIZE] = '\0';
+		isNullRecord = false;
+	}
+	usRecord.wChangeRecordCounter = 0;
+	usRecord.wRecordId = *dwRecordId;
+
+	SYSTEMTIME st = { 0 };
+	FILETIME ft = { 0 };
+	GetLocalTime(&st);
+	SystemTimeToFileTime(&st, &ft);
+	usRecord.fltmCreationTime = ft;
+
+	if (!WriteRecInFile(hRecordsFile, usRecord, sizeof(RECORDFILE_HEADER) + (*dwRecordId) * sizeof(USERRECORD)))
+		return false;
+
+	RECORDFILE_HEADER header = { 0 };
+	GetHead(hRecordsFile, 2, &header, FALSE);
+	header.wRecordFileSize += sizeof(USERRECORD);
+	header.wNotNullRecordsCount = isNullRecord ? header.wNotNullRecordsCount : ++header.wNotNullRecordsCount;
+	GetHead(hRecordsFile, 1, &header, FALSE);
+
+	(*dwRecordId)++;
+	(*dwFileSize) += sizeof(USERRECORD);
+
+	return true;
+}
+
+//Update record
+bool UpdateRec(HANDLE hRecordFile, DWORD  dwIdToModify)
+{
+	RECORDFILE_HEADER header;
+	CHAR cBuff[TEXTFIELD_SIZE + 1] = { 0 };
+
+	if (!GetHead(hRecordFile, 2, &header, FALSE))
+		return false;
+
+	cout << "Input NEW information (<80 symbs):\n";
+	cin >> cBuff;
+	cBuff[TEXTFIELD_SIZE] = '\0';
+
+	USERRECORD urBuff = { 0 };
+	DWORD dwBytes = 0;
+	BOOL isNullRecord = false;
+
+	if (SetFilePointer(hRecordFile, sizeof(RECORDFILE_HEADER) + sizeof(USERRECORD) * dwIdToModify, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+		return false;
+
+	if ((ReadFile(hRecordFile, &urBuff, sizeof(USERRECORD), &dwBytes, NULL) == false) || (dwBytes != sizeof(USERRECORD)))
+		return false;
+
+	isNullRecord = urBuff.cText[0] == '\0' ? true : false;
+
+	if (strcmp(cBuff, "0") == 0)
+		memset(urBuff.cText, '\0', sizeof(urBuff.cText));
+	else
+	{
+		strcpy_s(urBuff.cText, TEXTFIELD_SIZE, cBuff);
+		urBuff.cText[80] = '\0';
+	}
+	urBuff.wChangeRecordCounter++;
+
+	if (!WriteRecInFile(hRecordFile, urBuff, (sizeof(RECORDFILE_HEADER) + sizeof(USERRECORD) * dwIdToModify)))
+		return false;
+
+	if (isNullRecord && (strcmp(cBuff, "0") != 0)) header.wNotNullRecordsCount++;
+
+	if (!isNullRecord && (strcmp(cBuff, "0") == 0)) header.wNotNullRecordsCount--;
+
+	if (!GetHead(hRecordFile, 1, &header, FALSE))
+		return false;
+
+	return true;
+}
+
+//Delete record
+bool DeleteRec(HANDLE hRecordFile, DWORD dwIdToDelete, DWORD* dwCountRecords, DWORD* dwFileSize)
+{
+	RECORDFILE_HEADER header;
+	DWORD dwLastId = *dwCountRecords - 1;
+	CHAR cBuff[TEXTFIELD_SIZE + 1] = { 0 };
+
+	if (!GetHead(hRecordFile, 2, &header, FALSE))
+		return false;
+
+	USERRECORD urBuff = { 0 };
+	DWORD dwBytes = 0;
+
+	if (SetFilePointer(hRecordFile, sizeof(RECORDFILE_HEADER) + sizeof(USERRECORD) * dwLastId, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+		return false;
+
+	if ((ReadFile(hRecordFile, &urBuff, sizeof(USERRECORD), &dwBytes, NULL) == false) || (dwBytes != sizeof(USERRECORD)))
+		return false;
+
+	urBuff.wRecordId = dwIdToDelete;
+
+	if (!WriteRecInFile(hRecordFile, urBuff, (sizeof(RECORDFILE_HEADER) + sizeof(USERRECORD) * dwIdToDelete)))
+		return false;
+
+	header.wNotNullRecordsCount = urBuff.cText[0] == '\0' ? header.wNotNullRecordsCount : header.wNotNullRecordsCount - 1;
+	header.wRecordFileSize = header.wRecordFileSize - sizeof(USERRECORD);
+	(*dwCountRecords)--;
+	(*dwFileSize) -= sizeof(USERRECORD);
+
+	if (!GetHead(hRecordFile, 1, &header, FALSE))
+		return false;
+	return true;
+}
+
+//Get Header
+bool GetHead(HANDLE hRecordFile, DWORD dwCreateOrGetHeader, P_RECORDFILE_HEADER pHeader, BOOL bClearFile)
+{
+	DWORD dwBytes = 0;
+	if (SetFilePointer(hRecordFile, 0, 0, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+		return false;
+
+	if (dwCreateOrGetHeader == 1) //Создать заголовок
+	{
+		if ((WriteFile(hRecordFile, pHeader, sizeof(RECORDFILE_HEADER), &dwBytes, NULL) == false) ||
+			(dwBytes != sizeof(RECORDFILE_HEADER)))
+			return false;
+
+		if (bClearFile)
+		{
+			if (SetEndOfFile(hRecordFile) == false)
+				return false;
+		}
+	}
+	else if (dwCreateOrGetHeader == 2) //Считать заголовок
+	{
+		if ((ReadFile(hRecordFile, pHeader, sizeof(RECORDFILE_HEADER), &dwBytes, NULL) == false) ||
+			(dwBytes != sizeof(RECORDFILE_HEADER)))
+			return false;
+	}
+	else
+		return false;
+
+	return true;
+}
+
+bool ShowRec(HANDLE hRecordFile, DWORD dwCountRecords)
+{
+	RECORDFILE_HEADER header = { 0 };
+	USERRECORD recordsBuff = { 0 };
+	DWORD dwReadedBytes = 0;
+
+	if (!GetHead(hRecordFile, 2, &header, FALSE))
+		return false;
+
+	cout << "\n============= HEADER =============" << endl;
+	cout << "* Number of NotNull records: " << header.wNotNullRecordsCount << endl;
+	cout << "* File size: " << header.wRecordFileSize << " Bytes" << endl;
+
+	while ((ReadFile(hRecordFile, &recordsBuff, sizeof(USERRECORD), &dwReadedBytes, NULL) != false) && (dwReadedBytes == sizeof(USERRECORD)))
+	{
+		SYSTEMTIME st;
+		FileTimeToSystemTime(&recordsBuff.fltmCreationTime, &st);
+		cout << "============= RECORD =============" << endl;
+		cout << "* ID:" << recordsBuff.wRecordId << endl;
+		printf("* Creation date: %d-%d-%d, %d:%d\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute);
+		cout << "* Number of updates: " << recordsBuff.wChangeRecordCounter << endl;
+		cout << "* Text: " << recordsBuff.cText << endl;
+	}
+	return true;
+}
+
+int main()
+{
+
+	DWORD dwCountRecords = 0, dwFileSize = 0;
+	HANDLE hRecordFile = NULL;
+	BOOL bDeleteFlag = FALSE;
+
+	if (!InitProgData(&hRecordFile, &dwFileSize, &dwCountRecords))
+	{
+		cout << "Can't initialize file for record!" << endl;
+		return 1;
+	}
+
+	CHAR cKey;
+	for (;;) {
+		cout << "\nChoose operation:" << endl;
+		cout << " 1 - Create record" << endl;
+		cout << " 2 - Update record" << endl;
+		cout << " 3 - Delete record" << endl;
+		cout << " 4 - Get header" << endl;
+		cout << " 5 - ShutDown" << endl;
+		cout << "Your choice is: ";
+		cin >> cKey;
+		switch (cKey)
+		{
+		case '1': //Создать запись
+		{
+			CHAR Buff[TEXTFIELD_SIZE] = { 0 };
+			cout << "\nInput (<80 symbols) information:\n";
+			cin >> Buff;
+			if (sizeof(Buff) <= TEXTFIELD_SIZE) {
+				if (!CreateRec(hRecordFile, &dwFileSize, Buff, &dwCountRecords))
+					cout << "\nError! Can't add a new record";
+			}
+			else
+				cout << "\nError! Too many symbs!";
+		} break;
+
+		case '2': //Апдейт записи
+		{
+			DWORD dwIdToModify;
+			cout << "\nInput record ID to update: ";
+			cin >> dwIdToModify;
+			if (dwIdToModify >= dwCountRecords)
+			{
+				cout << "\nError! No such ID";
+				continue;
+			}
+			else
+			{
+				if (!UpdateRec(hRecordFile, dwIdToModify))
+				{
+					cout << "\nError! Can't update record";
+					continue;
+				}
+			}
+		} break;
+
+		case '3': //Удалить запись
+		{
+			DWORD dwIdToDelete;
+			cout << "\nInput record ID to delete: ";
+			cin >> dwIdToDelete;
+			if (dwIdToDelete >= dwCountRecords)
+			{
+				cout << "\nError! No such ID";
+				continue;
+			}
+			else
+			{
+				if (!DeleteRec(hRecordFile, dwIdToDelete, &dwCountRecords, &dwFileSize))
+					cout << "\nError! Can't delete record";
+				continue;
+			}
+		} break;
+
+		case '4': //Вывод заголовка и прочей инфы
+		{
+			if (!ShowRec(hRecordFile, dwCountRecords))
+			{
+				cout << "\nError! Can't show header";
+				continue;
+			}
+		} break;
+
+		case '5': //выход
+		{
+			CloseHandle(hRecordFile);
+			return 0;
+		} break;
+
+		default: {
+			cout << "\nWrong key value!";
+		} continue;
+		}
+	}
+}
