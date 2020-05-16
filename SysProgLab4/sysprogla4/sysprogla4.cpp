0ed4aa591e6f7f869c5f0eb9c9fb8543bfa21bd3
PolishukArtem <CazeOne1@gmail.com>
2020-05-16 16:12:50 +0300

    Load HW4

---
+++ SysProgLab4/sysprogla4/sysprogla4.cpp
@@ -0,0 +1,165 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include "windows.h"
+
+	void ArchieveFile(TCHAR* tFilePath, TCHAR* tArchivePath);
+	void GetFileFromArchieve(TCHAR* tArchivePath, TCHAR* tExtractArchivePath);
+	void Create7z(TCHAR* tCmdForNewProcess);
+	void ShowError();
+
+using namespace std;
+
+//Функция архивации файла
+void ArchieveFile(TCHAR* tFilePath, TCHAR* tArchivePath)
+{
+	TCHAR tCmdForNewProcess[MAX_PATH] = { '\0' };
+
+		printf("Command: ");
+		snprintf(tCmdForNewProcess, MAX_PATH, "%s%s %s", TEXT("7z.exe a "), tArchivePath, tFilePath);
+		cout << tCmdForNewProcess;
+
+	Create7z(tCmdForNewProcess);
+}
+
+//Функция создания 7z файла
+void Create7z(TCHAR* tCmdForNewProcess)
+{
+	HANDLE hServer = { NULL };
+	HANDLE hClient = { NULL };
+	SECURITY_ATTRIBUTES sa;
+
+		sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+		sa.bInheritHandle = TRUE;
+		sa.lpSecurityDescriptor = NULL;
+
+			if ((!CreatePipe(&hClient, &hServer, &sa, NULL)) ||
+				(hServer == INVALID_HANDLE_VALUE) || (hClient == INVALID_HANDLE_VALUE))
+				{
+					CloseHandle(hClient);
+					CloseHandle(hServer);
+					ShowError();
+					return;
+				}
+
+					STARTUPINFO si = { NULL };
+					si.cb = sizeof(si);
+					si.dwFlags |= STARTF_USESTDHANDLES;
+					si.hStdError = hServer;
+					si.hStdOutput = hServer;
+
+						PROCESS_INFORMATION pi = { NULL };
+
+							if (!CreateProcessA(NULL, tCmdForNewProcess, NULL, NULL, TRUE,
+								ABOVE_NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+								{
+									CloseHandle(hClient);
+									CloseHandle(hServer);
+									ShowError();
+									return;
+								}
+
+									WaitForSingleObject(pi.hProcess, INFINITE);
+									TerminateProcess(pi.hProcess, 0);
+
+										CloseHandle(hServer);
+
+											CloseHandle(pi.hThread);
+											CloseHandle(pi.hProcess);
+
+											DWORD dwReadedBytes = 0;
+											TCHAR tBuff[1024] = { '\0' };
+											BOOLEAN bAlrightFlag = TRUE;
+
+	while (ReadFile(hClient, tBuff, sizeof(tBuff) - 1, &dwReadedBytes, NULL) || (dwReadedBytes != 0))
+		{
+			printf("%s", tBuff);
+			if (strstr(tBuff, "Operation is SUCCESSFULL") == NULL) bAlrightFlag = FALSE;
+			memset(tBuff, '\0', sizeof(tBuff));
+		}
+
+			cout << "Parent process executioning: ";
+			if (!bAlrightFlag)
+				cout << "ERROR" << endl;
+			else
+				cout << "SUCCESS" << endl;
+
+					CloseHandle(hClient);
+}
+
+//Функция разорхивирования
+void GetFileFromArchieve(TCHAR* tArchivePath, TCHAR* tExtractArchivePath)
+{
+	TCHAR tCmdForNewProcess[MAX_PATH] = { '\0' };
+
+		printf("Command: ");
+		snprintf(tCmdForNewProcess, MAX_PATH, "%s%s -o\"%s\"", TEXT("7z.exe x "), tArchivePath, tExtractArchivePath);
+		cout << tCmdForNewProcess;
+
+	Create7z(tCmdForNewProcess);
+}
+
+int main()
+{
+	CHAR cKey;
+	for (;;)
+	{
+		cout << "\n============= MENU =============" << endl;
+		cout << " \t1 - Archieve" << endl;
+		cout << " \t2 - Get from archieve" << endl;
+		cout << " \t3 - Shut down" << endl;
+		cout << "Input key value: ";
+		cin >> cKey;
+
+				switch (cKey)
+				{
+
+						case '1': //Archieve [PATH\FILE] into [PATH]
+						{
+							TCHAR tPathToFiles[MAX_PATH] = { '\0' };
+							TCHAR tPathToNewArchive[MAX_PATH] = { '\0' };
+							cout << "\nInput file path to archieve: ";
+							scanf_s("%s", tPathToFiles, MAX_PATH);
+							cout << "Input path to save archieve: ";
+							scanf_s("%s", tPathToNewArchive, MAX_PATH);
+
+							ArchieveFile(tPathToFiles, tPathToNewArchive);
+						} break;
+
+						case '2': //Get file from [PATH\FILE] archieve into [PATH] 
+						{
+							TCHAR tPathToArchive[MAX_PATH] = { '\0' };
+							TCHAR tPathToExtractArchive[MAX_PATH] = { '\0' };
+							cout << "\nInput path to archieve file: ";
+							scanf_s("%s", tPathToArchive, MAX_PATH);
+							cout << "Input path to get file from archieve: ";
+							scanf_s("%s", tPathToExtractArchive, MAX_PATH);
+
+							GetFileFromArchieve(tPathToArchive, tPathToExtractArchive);
+						} break;
+
+						case '3': //Shut down
+						{ return 0; } break;
+
+
+						default: 
+						{
+							cout << "\nError! Wrong key value!";
+						} continue;
+				}
+	}
+}
+
+//Функция вывода ошибки
+void ShowError()
+{
+	LPVOID lpMsgBuf;
+	DWORD dwLastError = GetLastError();
+
+		FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwLastError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+
+			printf("\n%s\n", lpMsgBuf);
+			LocalFree(lpMsgBuf);
+
+	return;
+}
---
+++ SysProgLab4/sysproglab4.2/sysproglab4.2.cpp
@@ -0,0 +1,132 @@
+﻿// sysproglab4.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include "windows.h"
+
+#define ARRAY_VALUES_MIN 10
+#define ARRAY_VALUES_MAX 100
+#define ARRAY_SIZE 7 
+
+DWORD WINAPI StartNewThread(LPVOID param);
+
+void GenereteArray(int* arr);
+int CalculateLargestDivisor(int num);
+int CalculateSumOfArrayValues(int* arr);
+void PrintArray(int* arr, int length);
+
+using namespace std;
+
+int gdwTlsIndex;
+CRITICAL_SECTION gCriticalSection;
+HANDLE* threadHandles;
+
+
+DWORD WINAPI StartNewThread(LPVOID param)
+{
+	EnterCriticalSection(&gCriticalSection); //Пропуск только одного потока на выполнение
+	int maxDivisors[ARRAY_SIZE] = { 1 }; //выделяем память под значения
+	TlsSetValue(gdwTlsIndex, maxDivisors);
+
+		//Вывод всей инфы про то что в потоке
+		cout << "\n+===================| ID - " << GetCurrentThreadId() << " |===================-" << endl;
+		cout << "|Source array:\t\t ";
+		PrintArray((int*)param, ARRAY_SIZE);
+
+			for (int i = 0; i < ARRAY_SIZE; i++)
+				maxDivisors[i] = CalculateLargestDivisor(((int*)param)[i]);
+			cout << "|Greatest divisor:\t";
+			PrintArray((int*)TlsGetValue(gdwTlsIndex), ARRAY_SIZE);
+
+		cout << "|Sum of all elements:\t\t\t " << CalculateSumOfArrayValues((int*)param) << endl;
+		cout << "+====================================================-" << endl;
+	//Разрешение другим процессам на выполнение
+	LeaveCriticalSection(&gCriticalSection);
+	return 0;
+}
+
+//функция генерации массива
+void GenereteArray(int* arr)
+{
+	int randRange = ARRAY_VALUES_MAX - ARRAY_VALUES_MIN;
+
+		for (int i = 0; i < ARRAY_SIZE; i++)
+			arr[i] = (rand() % randRange) + ARRAY_VALUES_MIN;
+}
+
+//функция подсчёта максимального делителя
+int CalculateLargestDivisor(int num)
+{
+	int halfNum = ceil(num / 2), res = 1;
+
+		for (int i = 2; i <= halfNum; i++)
+			{
+				if (num % i == 0)
+					res = i;
+			}
+
+	return res;
+}
+
+//функция подсчёта суммы
+int CalculateSumOfArrayValues(int* arr)
+{
+	int sum = arr[0];
+
+		for (int i = 1; i < ARRAY_SIZE; i++)
+			sum += arr[i];
+				
+	return sum;
+}
+
+//функция вывода
+void PrintArray(int* arr, int length)
+{
+	for (int i = 0; i < length; i++)
+		cout << " " << arr[i] << " ";
+
+	cout << endl;
+}
+
+
+int main()
+{
+	int countOfThreads = 0;
+	cout << "Set up number of streams: ";
+	cin >> countOfThreads;
+
+		if (countOfThreads > 0)
+		{
+			int** arrOfArraysForNewThread;
+			arrOfArraysForNewThread = (int**)malloc(sizeof(int**) * countOfThreads);
+
+				for (int i = 0; i < countOfThreads; i++)
+					{
+						arrOfArraysForNewThread[i] = (int*)malloc(sizeof(int) * ARRAY_SIZE);
+						GenereteArray(arrOfArraysForNewThread[i]);
+					}
+
+						InitializeCriticalSection(&gCriticalSection);
+						gdwTlsIndex = TlsAlloc();
+						threadHandles = (HANDLE*)malloc(sizeof(HANDLE) * countOfThreads);
+						DWORD* dwThreadsID = (DWORD*)malloc(sizeof(DWORD) * countOfThreads);
+		
+			for (int i = 0; i < countOfThreads; i++)
+				threadHandles[i] = CreateThread(NULL, NULL, StartNewThread,
+					arrOfArraysForNewThread[i], NULL, &dwThreadsID[i]);
+
+		WaitForMultipleObjects(countOfThreads, threadHandles, TRUE, INFINITE);
+		}
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
